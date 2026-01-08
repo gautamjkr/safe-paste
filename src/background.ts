2c0c358ac4b3444ed1239b1e1e14a4d0b8217be4
Gautam Kumar <gautamjkr1995@gmail.com>
2026-01-08 05:34:15 +0530

    Created web page for safe-paste documentation

--- src/background.ts
+++
@@ -1,9 +0,0 @@
-// Background service worker for SafePaste
-// All PII detection is now done client-side in the content script
-// This file is kept minimal for Chrome extension requirements
-
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("🔵 SafePaste background service worker installed.");
-});
-
-
